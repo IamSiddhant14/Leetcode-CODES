64fb712ae12543c00f0266b0d06308b31db2287d
Siddhant Sharma <53156374+IamSiddhant14@users.noreply.github.com>
2022-03-01 17:15:30 +0530

    Time: 1 ms (99.97%), Space: 46 MB (55.37%) - LeetHub

---
+++ 338-counting-bits/338-counting-bits.java
@@ -0,0 +1,57 @@
+// class Solution {
+//     public int[] countBits(int n) {
+        
+//         int[] ans = new int[n+1];
+        
+//         for( int i =0; i<=n; i++){
+            
+//             int val = decToBinary(i);
+            
+//             int total = 0;
+            
+//             while( val != 0){
+//                 int r = val%10;
+//                 val = val/10;
+//                 total += r;
+//             } 
+            
+//             ans[i] = total;
+
+//         }
+        
+//         return ans;
+
+//     }
+    
+//     public static int decToBinary(int n ){
+        
+//         int power = 1;
+//         int total = 0;
+        
+//         while( n != 0 ){
+           
+//             int r = n%2;
+//             total += r*power;
+            
+//             n = n/2;
+//             power *= 10;
+//         }
+        
+//         return total;
+
+//     }
+// }
+
+
+class Solution {
+    public int[] countBits(int num) {
+        
+        int[]arr = new int[num+1];
+        
+        for(int n=1;n<=num;n++){
+            arr[n] = arr[(n&(n-1))]+1;
+        }
+        
+        return arr;
+    }
+}
