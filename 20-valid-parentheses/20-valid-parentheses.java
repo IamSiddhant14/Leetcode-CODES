de52e06d2e3c4de7d19f19a83f3d7e0111b7a4ff
Siddhant Sharma <53156374+IamSiddhant14@users.noreply.github.com>
2022-03-11 00:07:13 +0530

    Time: 2 ms (80.49%), Space: 42 MB (34.18%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.java
+++ 20-valid-parentheses/20-valid-parentheses.java
@@ -1,32 +1,38 @@
 class Solution {
     public boolean isValid(String s) {
+        
         Stack<Character> st = new Stack<>();
-        for(int i = 0; i<s.length(); i++){
-            char ch = s.charAt(i);
+        
+        for( int i =0; i<s.length() ; i++){
+            
+            char c = s.charAt(i);
+            
+            if( c == '(' || c == '{' || c == '['){ 
+                st.push(c);
+            }else{
+                
+                if( st.size() == 0 ){
+                    
+                     return false;
+                }else{
 
-            if(ch == '{' || ch == '[' || ch == '('){
-                st.push(ch);
-            }
-            else{
-                if(st.size() == 0)
-                    return false;
-                if(ch == '}' && st.peek() != '{')
-                    return false;
-                else if(ch == ']' && st.peek() != '[')
-                    return false;
-                else if(ch == ')' && st.peek() != '(' )
-                    return false;
-                else{
-                    st.pop();
+                    if( c == ')'  && st.peek() != '(' ){
+                       return false;
+                    }else if( c == '}'  && st.peek() != '{'  ){
+                        return false;
+                    }else if( c == ']'  && st.peek() != '['){
+                        return false;
+                    }else{
+                        st.pop();
+                    }
+                
                 }
-            } 
+
+            }
+            
         }
-        return st.size() == 0;
-        // if(st.size() == 0)
-        //     return true;
-        // else
-        //     return false;
-
-
+        
+        return (st.size() == 0);
+        
     }
-} 
+}
