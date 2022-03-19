1cd5f9670ca10f3b1038c3e28c1bd48203bc0d9b
Siddhant Sharma <53156374+IamSiddhant14@users.noreply.github.com>
2022-03-19 18:37:36 +0530

    Time: 1 ms (54.73%), Space: 42.6 MB (9.22%) - LeetHub

--- 678-valid-parenthesis-string/678-valid-parenthesis-string.java
+++ 678-valid-parenthesis-string/678-valid-parenthesis-string.java
@@ -1,86 +1,31 @@
-// class Solution {
-//     public boolean checkValidString(String s) {
-        
-//         Stack<Integer> br = new Stack<>();
-//         Stack<Integer> str = new Stack<>();
-        
-//         for( int i =0; i<s.length() ; i++){
-            
-//             char ch = s.charAt(i);
-            
-//             if(ch == '('){
-//                 br.push(i);
-                
-//             }else if( ch == '*' ){
-//                 str.push(i);
-                
-//             }else if( ch == ')'){
-                
-//                 if( br.size() == 0 ){
-                    
-//                     if( str.size() > 0 && str.peek() > i  ){
-//                         return false;
-                        
-//                     }else if(str.size() == 0 ){
-//                         return false;
-                        
-//                     }else if( str.size() > 0 && str.peek() < i   ){
-//                         str.pop();
-//                     }else return false;
-                    
-//                 }else{
-//                     br.pop();
-//                 }
-//             }
-            
-            
-//         }
-        
-//         if( br.size() > 0){
-            
-//             if( br.size() > str.size()){
-//                 return false;
-//             }
-                                        
-//             if( str.size() != 0 && br.peek() > str.peek() ){
-//                 return false;
-//             }
-              
-//         }
-        
-        
-//         return true ;
-
-//     }
-// }
-
-
-
-
-
-
-
-
 class Solution {
     public boolean checkValidString(String s) {
-        int cmin = 0, cmax = 0; // open parentheses count in range [cmin, cmax]
-        for (char c : s.toCharArray()) {
-            if (c == '(') {
+        int cmax = 0;
+        int cmin = 0;
+        for( char c : s.toCharArray()){
+
+            if( c == '(' ){
                 cmax++;
                 cmin++;
-            } else if (c == ')') {
-                cmax--;
-                cmin--;
-            } else if (c == '*') {
-                cmax++; // if `*` become `(` then openCount++
-                cmin--; // if `*` become `)` then openCount--
-                // if `*` become `` then nothing happens
-                // So openCount will be in new range [cmin-1, cmax+1]
+            }else if( c == ')' ){
+               cmax--;
+               cmin--;
+            }else if( c == '*'){
+               cmax++;
+               cmin--;
             }
-            if (cmax < 0) return false; // Currently, don't have enough open parentheses to match close parentheses-> Invalid
-                                        // For example: ())(
-            cmin = Math.max(cmin, 0);   // It's invalid if open parentheses count < 0 that's why cmin can't be negative
+            
+            if( cmax < 0 ){
+               return false; 
+            } 
+            
+            cmin = Math.max( cmin , 0);
+
         }
-        return cmin == 0; // Return true if can found `openCount == 0` in range [cmin, cmax]
+        
+        return (cmin == 0);
+        
+        
+        
     }
 }
