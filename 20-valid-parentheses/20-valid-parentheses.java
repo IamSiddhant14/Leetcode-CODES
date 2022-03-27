2b8e0e76f027bfb45822ba0933684568d891079d
Siddhant Sharma <53156374+IamSiddhant14@users.noreply.github.com>
2022-03-27 18:08:29 +0530

    Time: 3 ms (52.30%), Space: 42.4 MB (17.66%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.java
+++ 20-valid-parentheses/20-valid-parentheses.java
@@ -1,30 +1,49 @@
 class Solution {
     public boolean isValid(String s) {
         
-        Stack<Character> st = new Stack<>();
+        Stack <Character> st = new Stack<>();
         
-        for( int i =0; i<s.length() ; i++){
-            char ch = s.charAt(i);
+        for( char ele : s.toCharArray() ){
+           
             
-            if( ch == '(' || ch == '{' || ch == '['){
-                st.push(ch);
+            if( ele == '('  || ele == '[' || ele == '{' ){
+                st.push(ele);
+                
             }else{
-                if( st.size() == 0)
+                
+                if( st.size() == 0 ){
                     return false;
-                else if( ch == ')' && st.peek() != '(')
-                    return false;
-                else if( ch == '}' && st.peek() != '{')
-                    return false;
-                else if( ch == ']' && st.peek() != '[')
-                    return false;
-                else{
-                    st.pop();
                 }
+                
+                if( ele == ')' ){
+                
+                      if(st.peek() != '('){
+                         return false;
+                      }
+
+                }else if( ele == ']'){
+                
+                     if(st.peek() != '['){
+                         return false;
+                      }
+                
+                }else if( ele == '}'){
+                
+                      if(st.peek() != '{'){
+                           return false;
+                       }
+                
+                
+                }
+                
+                st.pop();
+                
             }
-              
+       
+        
         }
         
-        return st.size() == 0;
-        
+        return st.size()== 0;
     }
+    
 }
