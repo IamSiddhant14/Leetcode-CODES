039769e9ffe99fce2618bf8d50280df48b5d35d0
Siddhant Sharma <53156374+IamSiddhant14@users.noreply.github.com>
2022-03-30 11:16:01 +0530

    Time: 14 ms (46.01%), Space: 55.5 MB (34.25%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.java
+++ 56-merge-intervals/56-merge-intervals.java
@@ -1,52 +1,51 @@
 class Solution {
     public int[][] merge(int[][] arr) {
         
-        Stack<int[]> st = new Stack<>();
-        
-        Arrays.sort(arr, (a,b)->{
-            
+        Arrays.sort(arr, (a,b) -> {
             return a[0] - b[0];
+        
         });
         
+        Stack<int[]> st = new Stack<>();
+        
         st.push(arr[0]);
-        
-        
         
         for( int i =1; i<arr.length ; i++){
             
             int[] a = st.peek();
             int[] b = arr[i];
             
-            if( a[1]>= b[0] ){ //This check is to detemine whether there was an interation or not
-            
+            if( a[1] >= b[0] ){
+                
                 int[] ans = new int[2];
                 
-                ans[0] = Math.min(a[0],b[0]);
-                ans[1] = Math.max(a[1],b[1]);
+                ans[0] = Math.min( a[0], b[0]);
+                ans[1] = Math.max(a[1] , b[1]);
                 
                 st.pop();
                 st.push(ans);
-                
             }else{
                 st.push(arr[i]);
             }
             
-            
         }
         
-        int[][] a = new int[st.size()][2];
+        int[][] an = new int[st.size()][2];
+        
         int i =0;
         
         while( st.size() != 0 ){
             
             int[] e = st.pop();
             
-            a[i][0] = e[0];
-            a[i][1] =e[1] ;
+            an[i][0] = e[0];
+            an[i][1] = e[1];
             i++;
+            
         }
         
-        return a;
+        return an;
+        
         
         
     }
