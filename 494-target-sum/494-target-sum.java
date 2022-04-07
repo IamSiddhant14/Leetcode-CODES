6999468bcead62eee4a206c213636c8518bfbed6
Siddhant Sharma <53156374+IamSiddhant14@users.noreply.github.com>
2022-04-07 21:36:17 +0530

    Time: 568 ms (30.97%), Space: 40 MB (93.55%) - LeetHub

--- 494-target-sum/494-target-sum.java
+++ 494-target-sum/494-target-sum.java
@@ -1,17 +1,30 @@
 class Solution {
     public int findTargetSumWays(int[] nums, int target) {
+        
         int[]count = new int[1];
-        findTargetSum(nums, 0, 0, target, count);
+        List<Integer>temp = new ArrayList<>();
+        
+        findTargetSum(nums, 0, 0, target, count, temp);
         return count[0];
+        
     }
-    public void findTargetSum(int[]nums, int idx, int ssf, int target, int[]count){
+    public void findTargetSum(int[]nums, int idx, int ssf, int target, int[]count, List<Integer>temp){
+        
         if(idx == nums.length){
             if(ssf == target){
+                // System.out.println(temp);
                 count[0]++;
             }
             return;
         }
-        findTargetSum(nums, idx + 1, ssf + nums[idx], target, count);
-        findTargetSum(nums, idx + 1, ssf - nums[idx], target, count);
+        
+        // temp.add(nums[idx]);
+        findTargetSum(nums, idx + 1, ssf + nums[idx], target, count, temp);
+        // temp.remove(temp.size() - 1);
+        
+        // temp.add(-1 * nums[idx]);
+        findTargetSum(nums, idx + 1, ssf - nums[idx], target, count, temp);
+        // temp.remove(temp.size() - 1);
+        
     }
 }
