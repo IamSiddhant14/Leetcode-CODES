60c5b38f3a820d03b2ad39d70a46503ac961810a
Siddhant Sharma <53156374+IamSiddhant14@users.noreply.github.com>
2022-04-09 16:55:57 +0530

    Time: 2 ms (24.11%), Space: 42.5 MB (20.14%) - LeetHub

--- 925-long-pressed-name/925-long-pressed-name.java
+++ 925-long-pressed-name/925-long-pressed-name.java
@@ -1,30 +1,30 @@
 class Solution {
     public boolean isLongPressedName(String name, String typed) {
         
-        if( name.length() > typed.length()){
-                return false;
+        if( name.length() > typed.length() ){
+            return false;
         }
         
         int i = 0;
         int j = 0;
         
-        while( i < name.length()  && j < typed.length()
- ){
-            
-            if( name.charAt(i) == typed.charAt(j) ){
-                i++;
-                j++;
-            }else if( name.charAt(i) != typed.charAt(j) ){
-                
-                 if(  i -1 >= 0 && name.charAt(i-1) == typed.charAt(j) ){
-                     j++;
-                 }else{
-                    return false;
-                 }
-            }
-
-        
-        }
+       while( i < name.length() && j < typed.length() ){
+           
+           if( name.charAt(i) == typed.charAt(j) ){
+               i++;
+               j++;
+               
+           }else if( name.charAt(i) != typed.charAt(j) ){
+               
+               if( i-1 >= 0 && name.charAt(i-1) == typed.charAt(j) ){
+                   j++;
+               }else{
+                   return false;
+               }
+                 
+           }
+             
+       }
         
         while( j < typed.length() ){
             
@@ -33,10 +33,9 @@ class Solution {
             }
             
             j++;
-            
+        
         }
         
-        return i < name.length() ? false : true;
-        
+       return i == name.length() ? true : false;
     }
 }
