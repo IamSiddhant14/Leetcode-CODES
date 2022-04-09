5968ffa3dbae934ce152f26d89c8032ed5106345
Siddhant Sharma <53156374+IamSiddhant14@users.noreply.github.com>
2022-04-10 02:07:21 +0530

    Time: 5 ms (59.68%), Space: 44.7 MB (18.09%) - LeetHub

--- 496-next-greater-element-i/496-next-greater-element-i.java
+++ 496-next-greater-element-i/496-next-greater-element-i.java
@@ -1,44 +1,49 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
         
-        int[] a = ngl(nums2);
-        HashMap<Integer, Integer> hm = new HashMap<>();
+        int[] a = getNGR( nums2 );
         
-        for( int i =0; i<nums2.length; i++){
-            hm.put(nums2[i] , a[i]);
+        HashMap<Integer , Integer> hm = new HashMap<>();
+        
+        for( int i = 0; i<nums2.length ; i++ ){
+
+            hm.put( nums2[i] , a[i] );
         }
         
-        for( int i =0; i<nums1.length ; i++){
-            nums1[i] = hm.get(nums1[i]);
+        int[] ans = new int[nums1.length];
+        
+        for( int j =0; j<nums1.length ; j++ ){
+            
+            ans[j] = hm.get(nums1[j]);
         }
         
-        return nums1;
+        return ans;
         
         
     }
     
-    
-    public static int[] ngl( int[] arr ){
+    public int[] getNGR ( int[] nums2 ){
+        
+        int n = nums2.length;
         
         Stack<Integer> st = new Stack<>();
-        int[] ans = new int[arr.length];
+        int[] ans = new int[n];
         
         Arrays.fill(ans , -1);
         
-        for( int i = 0; i<arr.length ; i++){
+        for( int i = 0; i<n ; i++ ){
             
-            while( st.size() > 0 && arr[st.peek()] < arr[i] ){
+            while( st.size() > 0 && nums2[st.peek()] < nums2[i]  ){
                 
                 int idx = st.pop();
-                ans[idx] = arr[i];
-                
+                ans[idx] = nums2[i];
             }
             
             st.push(i);
-
         }
         
         return ans;
+        
         
     }
 }
