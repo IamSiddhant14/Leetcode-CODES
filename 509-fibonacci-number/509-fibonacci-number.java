395404a7dedd14f08276beedd0069edef4426444
Siddhant Sharma <53156374+IamSiddhant14@users.noreply.github.com>
2022-04-24 10:09:45 +0530

    Time: 0 ms (100.00%), Space: 41.4 MB (14.82%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.java
+++ 509-fibonacci-number/509-fibonacci-number.java
@@ -7,8 +7,8 @@ class Solution {
         // int[] dp = new int[n+1];
         
         // dp[0] = 0; dp[1] = 1;
-        int curr = 1;
         int prev = 0;
+        int curr = 1;
         int forward = 0;
         
         for( int i =2; i<=n ; i++ ){
