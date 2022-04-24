ac41dec4483e446fb2848ae77c216de3601645cf
Siddhant Sharma <53156374+IamSiddhant14@users.noreply.github.com>
2022-04-24 06:49:22 +0530

    Time: 10 ms (26.47%), Space: 41.3 MB (19.31%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.java
+++ 509-fibonacci-number/509-fibonacci-number.java
@@ -1,27 +1,17 @@
 class Solution {
-    
-    public int[] fi( int n , int[] dp ){
-        
-        for( int i =2 ; i<dp.length ;i++ ){
-             dp[i] = dp[i-1] + dp[i-2];
-        }
-
-        return dp;
-    }
-    
     public int fib(int n) {
         
-        if( n == 0 )return 0;
-        if( n == 1 )return 1;
+     if( n == 0 ){
+return 0;
+     }
+        if( n == 1 ){
+ return 1;
+        }
         
-        int[] dp = new int[n+1];
-        dp[0] = 0;
+        int n1 = fib(n-1);
+        int n2 = fib(n-2);
         
-        dp[1] = 1;
-        int[] ans = fi( n , dp );
-        
-        return ans[n];
-        
+        return n1+n2;
         
     }
 }
