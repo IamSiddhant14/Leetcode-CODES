d2b700a0966be2ba1702b06f998a3d372f9de327
Siddhant Sharma <53156374+IamSiddhant14@users.noreply.github.com>
2022-05-01 21:32:10 +0530

    Time: 195 ms (12.73%), Space: 50.3 MB (9.54%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.java
+++ 45-jump-game-ii/45-jump-game-ii.java
@@ -1,34 +1,31 @@
 class Solution {
     public int jump(int[] nums) {
         
-        int n = nums.length;
-        long[] dp = new long[n+1];
+        long[] dp = new long[nums.length+1];
         Arrays.fill( dp , -1);
         
-        return getAns( 0 , dp , nums );
+        return (int)memorize( 0 , dp , nums);
         
     }
     
-    public int getAns( int n , long[] dp , int[] nums ){
+    public static long memorize(int n , long[] dp , int[] nums){
         
-        if( n == nums.length -1 ){
-             return 0;
-        }
-         
-        long ans = (long)(1e9);
+        if( n == nums.length -1 ) return 0;
         
-        if( dp[n] != -1 ) return (int)(dp[n]);
+        if( dp[n] != -1) return dp[n];
         
-        for( int i = 1; i<= nums[n] ; i++ ){
+        long ans = Integer.MAX_VALUE;
+        
+        for( int i =1; i<=nums[n] ; i++){
             
-            if( n+i <= nums.length-1 ){
-                  ans = Math.min(ans , getAns(n+i , dp , nums)+1 );
+            if( n+i <= nums.length -1 ){
+                ans = Math.min( ans , memorize( n+i , dp , nums )+1);
             }
-
         }
         
-        dp[n] = (int)(ans);
-        return (int)(ans);
-                  
+        dp[n] = ans;
+        
+        return ans;
+        
     }
 }
