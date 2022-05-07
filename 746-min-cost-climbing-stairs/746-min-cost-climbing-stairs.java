c3d373f93e4f6e024146cc6731aaf60c8730c13e
Siddhant Sharma <53156374+IamSiddhant14@users.noreply.github.com>
2022-05-07 20:50:09 +0530

    Time: 2 ms (30.06%), Space: 43.7 MB (49.07%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.java
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.java
@@ -1,31 +1,29 @@
 class Solution {
-    
     public int minCostClimbingStairs(int[] cost) {
         
-        int[] dp = new int[cost.length+1];
-        Arrays.fill( dp , -1 );
+        int[] dp = new int[cost.length+1] ;
+        Arrays.fill( dp , Integer.MAX_VALUE );
         
-        help( 0 , cost , dp );
+        int a1 = help( 0 , cost , dp );
+        int a2 = help( 1 , cost , dp );
         
-        return Math.min( dp[0] , dp[1] );
+        return Math.min( a1 , a2 );
+        
     }
     
-    public int help (int n , int[] cost , int[] dp  ){
-        
-//         if( n == cost.length-1 ){
-//            return cost[n];
-//         }
-        
-        if( n >= cost.length ){
-           return 0;
+    public int help( int n , int[] cost , int[] dp ){
+        if(n > cost.length){
+            return Integer.MAX_VALUE;
         }
+        if( n == cost.length ) return 0 ;
         
-        if( dp[n] != -1 ) return dp[n];
+        if( dp[n] != Integer.MAX_VALUE ) return dp[n];
         
-        int a1 = ( n+1 <= cost.length - 1 ) ? help( n+1 , cost , dp ) : 0;
-        int a2 = ( n+2 <= cost.length - 1 ) ? help( n+2 , cost , dp ) : 0;
-        
-        return dp[n] = Math.min( a1 , a2 ) + cost[n];
-
+        int a1 = help( n+1 , cost , dp );
+        int a2 = help( n+2 , cost , dp );
+        if(a1 == Integer.MAX_VALUE && a2 == Integer.MAX_VALUE){
+            return dp[n] = Integer.MAX_VALUE;
+        }
+        return dp[n] = Math.min(a1 , a2)+ cost[n];
     }
 }
