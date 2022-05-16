1eee731dca1cd5ba7e02a1c0fe2ac2c5e899055b
Siddhant Sharma <53156374+IamSiddhant14@users.noreply.github.com>
2022-05-16 19:40:55 +0530

    Time: 0 ms (100.00%), Space: 41.2 MB (28.27%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.java
+++ 70-climbing-stairs/70-climbing-stairs.java
@@ -2,26 +2,25 @@ class Solution {
     public int climbStairs(int n) {
         
         int[] dp = new int[n+1];
-        Arrays.fill( dp , -1 );
+        Arrays.fill( dp , -1);
         
-        return help( n , dp);   
+        return help( n , dp );
+    
     }
     
     public int help( int n , int[] dp ){
         
         if( n == 0 ){
+            //No of ways is still one in that manner
             return 1;
         }
         
         if( dp[n] != -1 ) return dp[n];
+                                    
+        int a1 = ( n-1 >= 0 ) ? help( n-1 , dp ) : 0;
+        int a2 = ( n-2 >= 0 ) ? help( n-2 , dp ) : 0;
         
-        int a1 = (n-1 >= 0 ) ? help(n-1 , dp) : 0;
-        int a2 = (n-2 >= 0 ) ? help(n-2 , dp) : 0;
-        
-        dp[n] = a1+a2;
-        return dp[n];
-        
+        return dp[n] = a1+a2;
+                                    
     }
-    
-    
 }
