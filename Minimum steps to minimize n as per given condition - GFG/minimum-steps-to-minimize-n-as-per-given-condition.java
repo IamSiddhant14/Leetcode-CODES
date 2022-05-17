998cf02eb756f32a01cfec6ae011f658b47fc742
Siddhant Sharma <53156374+IamSiddhant14@users.noreply.github.com>
2022-05-17 16:13:37 +0530

    Added solution - LeetHub

--- Minimum steps to minimize n as per given condition - GFG/minimum-steps-to-minimize-n-as-per-given-condition.java
+++ Minimum steps to minimize n as per given condition - GFG/minimum-steps-to-minimize-n-as-per-given-condition.java
@@ -22,30 +22,28 @@ class GfG
 
 class Solution{
 
-	public int minSteps(int n) 
+	public int minSteps(int N) 
 	{ 
-	    
-	    int[] dp = new int[n+1];
+	    int[] dp = new int[N+1];
 	    Arrays.fill( dp , -1);
 	    
-	    return help( n , dp);
-	   // return dp[n];
-   
+	    return help( N , dp );
+	    
 	} 
 	
-	public int help ( int n , int[] dp ){
+	public int help( int n , int[] dp ){
 	    
 	    if( n == 1 ) return 0;
 	    
-	    if( dp[n] != -1 ) return dp[n] ;
+	    if( dp[n] != -1 ) return dp[n];
 	    
-	    int a1 = ( n % 2 == 0 ) ? help(n/2 , dp) : Integer.MAX_VALUE ;
-	    int a2 = ( n % 3 == 0 ) ? help(n/3 , dp) : Integer.MAX_VALUE ;
-	    int a3 = help( n-1 , dp);
+	    int a1 = ( n % 2 == 0 ) ? help( n/2 , dp ) : Integer.MAX_VALUE;
+	    int a2 = ( n % 3 == 0 ) ? help( n/3 , dp ) : Integer.MAX_VALUE;
+	    int a3 = ( n - 1 >= 1 ) ? help( n-1 , dp ) : Integer.MAX_VALUE;
 	    
-	    int a4 = Math.min(a1 , a2 );
-	    
-	    return dp[n] = Math.min(a4, a3) + 1;
-	    
+	    return dp[n] = Math.min( a1 ,Math.min(a2 , a3))+1;
+	  
 	}
+	
+	
 }
