1174d3898a6ba88019b97bd4b0a8bd8b383fd16b
Siddhant Sharma <53156374+IamSiddhant14@users.noreply.github.com>
2022-06-04 17:39:46 +0530

    Time: 315 ms (8.44%), Space: 50.9 MB (5.04%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.java
+++ 45-jump-game-ii/45-jump-game-ii.java
@@ -1,31 +1,37 @@
 class Solution {
     public int jump(int[] nums) {
         
+        if( nums.length == 1 )return 0;
+        
+        
         long[] dp = new long[nums.length+1];
-        Arrays.fill( dp , -1);
+        Arrays.fill( dp , -1 );
         
-        return (int)help( 0 , nums , dp );
-        
+        ansA( nums , dp , 0 );
+        return (int)dp[0];
+
     }
     
-    public long help( int idx , int[] nums , long[] dp ){
+    public long ansA ( int[] nums , long[] dp , int j){
         
-        if( idx >= nums.length ) return Integer.MAX_VALUE;
-        
-        if( idx == nums.length - 1) return 0;
-        
-        if( dp[idx] != -1 ) return dp[idx];
-        
-       long ans = Integer.MAX_VALUE;
-
-        for( int i=1; i<= nums[idx]; i++){
-            
-            // if( idx+i <= nums.length - 1 ){
-                ans = Math.min( ans , help(idx+i , nums , dp)+1l);
-            // }
+        if( j >= nums.length-1 ){
+            return 0;
         }
         
-        return dp[idx] = ans;
- 
+        if( dp[j] != -1 ) return dp[j];
+        long ans = Integer.MAX_VALUE;
+        
+        for( int i = 1; i <= nums[j] ; i++ ){
+            
+            if( j+i <= nums.length-1 ){
+               ans = Math.min( ans , ansA( nums , dp , j+i ) + 1l);
+            }
+            
+        } 
+        
+        dp[j] = ans;
+        
+        return dp[j] ;
+        
     }
 }
