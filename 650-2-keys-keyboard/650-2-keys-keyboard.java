a5ee7c86e1e100a4569e4b416a4d9b5ea835727f
Siddhant Sharma <53156374+IamSiddhant14@users.noreply.github.com>
2022-06-05 09:50:07 +0530

    Time: 423 ms (5.02%), Space: 331.6 MB (5.43%) - LeetHub

--- 650-2-keys-keyboard/650-2-keys-keyboard.java
+++ 650-2-keys-keyboard/650-2-keys-keyboard.java
@@ -1,35 +1,41 @@
 class Solution {
     public int minSteps(int n) {
         
-         if( n == 1 ){
-          return 0;
-        }
+         if( n == 1 ) return 0;
         
         long[][] dp = new long[2*n + 1][2*n + 1];
         
-        for( int i = 0; i< 2*n + 1 ; i++){
-            for( int j = 0; j< 2*n + 1 ; j++){
+        for( int i = 0; i<dp.length ; i++){
+            for( int j = 0; j<dp[0].length ; j++ ){
                 dp[i][j] = -1;
             }
         }
-        
-        return (int)(ans( 1 , 1 , n , dp )+1l);
+
+        return (int)(ans( 1 , 1 , n , dp ) + 1l) ;
         
     }
     
-    public long ans( int screen , int buffer , long dest , long[][] dp ){
-
-        if( screen == dest ) return 0;
-        if( screen > dest )  return Integer.MAX_VALUE;
+    public long ans( int screen , int buffer , int dest , long[][] dp )
+    {
         
-        if( dp[screen][buffer] != -1 ) return dp[screen][buffer];
+        if( screen == dest ) return 0;
+        
+        if( screen > dest ) return Integer.MAX_VALUE;
+        
+        if( dp[screen][buffer]  != -1 ) return dp[screen][buffer] ;
         
         long copyPaste = 2l + ans( 2*screen , screen , dest , dp );
-        long paste = 1l + ans( screen + buffer , buffer , dest , dp );
+        long paste = 1l + ans( screen+buffer , buffer , dest , dp );
         
-        dp[screen][buffer] = Math.min( copyPaste , paste );
+        dp[screen][buffer] = Math.min( copyPaste , paste ) ;
         
         return dp[screen][buffer];
-        
+
     }
+    
+    
+    
+    
+    
+    
 }
