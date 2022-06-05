0fa9651e90b2678453f0509830ee8c495a2be74f
Siddhant Sharma <53156374+IamSiddhant14@users.noreply.github.com>
2022-06-05 21:03:09 +0530

    Time: 1 ms (96.43%), Space: 42.3 MB (54.28%) - LeetHub

--- 91-decode-ways/91-decode-ways.java
+++ 91-decode-ways/91-decode-ways.java
@@ -1,39 +1,34 @@
 class Solution {
-    
-    
     public int numDecodings(String s) {
         
-        int[] dp = new int[s.length() + 1 ];// For the corresponding length(index) it will be storing the respective no of encodings possible for a string of that given length
-        
+        int[] dp = new int[s.length()+1];
         Arrays.fill( dp , -1 );
-        return ans( s , 0 , dp );
         
+        return ans( 0 , s , dp );
     }
     
-    public int ans( String s , int idx , int[] dp ){
+    public int ans ( int idx , String s , int[] dp ){
         
-        if( idx == s.length() )return 1; // As a empty could be encoded in one way
+        if( idx == s.length() ) return 1;// As a empty could be encoded in one way
         
         if( dp[idx] != -1 ) return dp[idx];
         
         int ans1 = 0 , ans2 = 0;
         
-        if( s.charAt(idx) != '0'){
-            ans1 = ans( s , idx+1 , dp );
+        if( s.charAt(idx) != '0' ){
+            ans1 = ans( idx+1 , s , dp );
         }
         
-        if( idx+1 < s.length() && s.charAt(idx) != '0' ){
-            int digit = ( (s.charAt(idx) - '0')* 10  + ( s.charAt(idx+1) - '0') );
-            
-            if( digit >= 10 && digit <= 26  ){
-                ans2 = ans( s , idx+2 , dp );
+        if( idx + 1 < s.length() &&  s.charAt(idx) != '0' ){
+            int a = (s.charAt(idx) - '0') *10 + (s.charAt(idx+1) - '0');
+            if( a >= 10 && a <= 26 ){
+                ans2 = ans( idx+2 , s , dp );
             }
         }
+         
+        dp[idx] = ans1 + ans2 ;
         
-        return dp[idx] = ans1+ans2;
-        
+        return dp[idx];
+
     }
-    
-    
-    
 }
