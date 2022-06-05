856d78efa7587155d3c6e6fc0eba7d7f6fec8412
Siddhant Sharma <53156374+IamSiddhant14@users.noreply.github.com>
2022-06-05 14:56:43 +0530

    Time: 1 ms (96.43%), Space: 40.4 MB (95.98%) - LeetHub

--- 91-decode-ways/91-decode-ways.java
+++ 91-decode-ways/91-decode-ways.java
@@ -1,40 +1,39 @@
 class Solution {
     
-    public int memo( String s , int idx , int[] dp ){
-        
-        if( s.length() == idx ) return 1;
-        
-        if( dp[idx] != -1 ) return dp[idx];
-        
-        int ans1 = 0, ans2 = 0;
-        
-        if( s.charAt(idx) != '0' ){
-            ans1 = memo( s , idx+1 , dp );
-        }
-        
-        if( idx+1 < s.length() ){
-            
-            int code = (s.charAt(idx) - '0') * 10 + (s.charAt(idx+1) - '0') ;
-            
-            if(  code >= 10 &&  code <= 26 ){
-                
-                ans2 = memo( s , idx+2 , dp);
-                
-            }
-            
-        }
-        
-        return dp[idx] = ans1+ans2 ;
-        
-    }
-    
     
     public int numDecodings(String s) {
         
-        int[] dp = new int[s.length()+1];
-        Arrays.fill( dp , -1);
+        int[] dp = new int[s.length() + 1 ];// For the corresponding length(index) it will be storing the respective no of encodings possible for a string of that given length
         
-        return memo( s, 0 , dp);
+        Arrays.fill( dp , -1 );
+        return ans( s , 0 , dp );
         
     }
+    
+    public int ans( String s , int idx , int[] dp ){
+        
+        if( idx == s.length() )return 1; // As a empty could be encoded in one way
+        
+        if( dp[idx] != -1 ) return dp[idx];
+        
+        int ans1 = 0 , ans2 = 0;
+        
+        if( s.charAt(idx) != '0'){
+            ans1 = ans( s , idx+1 , dp );
+        }
+        
+        if( idx+1 < s.length() && s.charAt(idx) != '0' ){
+            int digit = ( (s.charAt(idx) - '0')* 10  + ( s.charAt(idx+1) - '0') );
+            
+            if( digit >= 10 && digit <= 26  ){
+                ans2 = ans( s , idx+2 , dp );
+            }
+        }
+        
+        return dp[idx] = ans1+ans2;
+        
+    }
+    
+    
+    
 }
