09a9ddc45fe9399855394aa495f2b7012a9d07d3
Siddhant Sharma <53156374+IamSiddhant14@users.noreply.github.com>
2022-06-07 11:12:33 +0530

    Added solution - LeetHub

--- Consecutive 1's not allowed - GFG/consecutive-1s-not-allowed.java
+++ Consecutive 1's not allowed - GFG/consecutive-1s-not-allowed.java
@@ -20,14 +20,14 @@ class GFG {
     }
 }// } Driver Code Ends
 
-
 // User function Template for Java
 
 class Solution {
     long countStrings(int n) {
         
-        long[][] dp = new long[n+1][2];
-        for( int i = 0; i<dp.length ; i++ ){
+        long[][] dp = new long [n+1][2];
+        
+        for( int i = 0; i < dp.length ; i++){
             dp[i][0] = -1 ;
             dp[i][1] = -1 ;
         }
@@ -37,16 +37,15 @@ class Solution {
     
     long ans( int n , int prev , long[][] dp ){
         
-        if( n == 0 ) return 1;
+        if( n == 0 ) return 1 ;
         
-        if( dp[n][prev] != -1 ) return dp[n][prev] ;
+        if( dp[n][prev] != -1 ) return  dp[n][prev];
         
         long append0 = ans( n-1 , 0 , dp );
-        long append1 = ( prev != 1 ) ? ans( n-1 , 1 , dp ) :  0;
+        long append1 = (prev != 1 ) ? ans( n-1 , 1 , dp ) : 0;
         
-        dp[n][prev] = ( append0 + append1 ) % 1000000007 ;
-        
-        return dp[n][prev] ;
-        
+        dp[n][prev] = ( append0 + append1 ) % 1000000007 ; 
+         
+        return  dp[n][prev];
     }
 }
