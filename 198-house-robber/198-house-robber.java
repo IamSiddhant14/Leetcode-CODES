fec26e6e3cd38d2aa664b675d0bc1d4e0cf5ca69
Siddhant Sharma <53156374+IamSiddhant14@users.noreply.github.com>
2022-06-08 17:06:36 +0530

    Time: 0 ms (100.00%), Space: 41.7 MB (35.19%) - LeetHub

--- 198-house-robber/198-house-robber.java
+++ 198-house-robber/198-house-robber.java
@@ -3,25 +3,29 @@ class Solution {
         
         int[][] dp = new int[nums.length+1][2];
         
-        for( int i = 0 ; i<= nums.length ; i++ ){
+        for( int i = 0; i<dp.length ; i++ ){
             dp[i][0] = -1;
             dp[i][1] = -1;
         }
         
-       return  ans( nums , dp , 0 , 0 );
+        return ans( nums , dp , 0  , 0);
+        
         
     }
     
-    public int ans( int[] nums , int[][] dp , int taken , int idx ){
+    public int ans( int[] nums , int[][] dp , int prev , int idx ){
         
-        if( idx == nums.length ) return 0;
+        if( idx == nums.length ){
+            return 0;
+        }
         
-        if( dp[idx][taken] != -1 ) return dp[idx][taken];
-        
-        int a1 = ( taken == 0 ) ? ans( nums , dp , 1 , idx+1 ) + nums[idx] : 0;
-        int a2 = ans( nums , dp , 0 , idx+1 );
-        
-        dp[idx][taken] = Math.max( a1 , a2 );
-        return dp[idx][taken];
+        if( dp[idx][prev] != -1 )  return dp[idx][prev];
+                                                  
+        int a1 = ( prev == 0 ) ? ans( nums , dp , 1 , idx+1 )+ nums[idx] : Integer.MIN_VALUE;
+        int a2 = ans( nums , dp , 0 , idx+1 ) ;
+                                                  
+        dp[idx][prev] = Math.max( a1 , a2 );
+        return dp[idx][prev] ;
+                                                  
     }
 }
