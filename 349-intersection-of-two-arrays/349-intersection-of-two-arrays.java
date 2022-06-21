575af5267495fc6d4e1e5d9e085588c0fedac6aa
Siddhant Sharma <53156374+IamSiddhant14@users.noreply.github.com>
2022-06-22 01:00:12 +0530

    Time: 4 ms (69.47%), Space: 43.9 MB (52.16%) - LeetHub

--- 349-intersection-of-two-arrays/349-intersection-of-two-arrays.java
+++ 349-intersection-of-two-arrays/349-intersection-of-two-arrays.java
@@ -1,37 +1,29 @@
 class Solution {
     public int[] intersection(int[] nums1, int[] nums2) {
         
-        HashMap<Integer , Integer> hm1 = new HashMap<>();
-        HashMap<Integer , Integer> hm2 = new HashMap<>();
+        HashMap<Integer , Integer> hm = new HashMap<>();
+        
+        for( int i = 0; i<nums1.length ; i++){
+            hm.put(nums1[i] ,hm.getOrDefault(nums1[i] , 0) + 1 );
+        }
         
         ArrayList<Integer> al = new ArrayList<>();
         
-        for( int ele : nums1 ){
-            hm1.put( ele , hm1.getOrDefault(ele , 0)+1);
-        }
-        System.out.println(hm1);
-        
-        for( int ele : nums2 ){
-            hm2.put( ele , hm2.getOrDefault(ele , 0)+1);
-        }
-        
-        for( int ele : hm1.keySet()){
-            
-            if( hm2.containsKey(ele) ){
-                
-                al.add(ele);
-                hm2.remove(ele);
-
+        for( int i = 0 ; i<nums2.length ; i++ ){
+            if( hm.containsKey( nums2[i] ) ){
+                al.add(nums2[i]);
+                hm.remove(nums2[i]);
             }
         }
         
         int[] ans = new int[al.size()];
-        
         for( int i = 0; i<ans.length ; i++ ){
-           ans[i] = al.get(i);
+            ans[i] = al.get(i);
         }
         
         return ans;
+        
+        
         
     }
 }
