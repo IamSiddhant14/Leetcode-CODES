b6714cc857c9885dfc25f3deb2f508416383c93d
Siddhant Sharma <53156374+IamSiddhant14@users.noreply.github.com>
2022-06-22 18:58:52 +0530

    Time: 1 ms (10.70%), Space: 40.9 MB (49.12%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.java
+++ 70-climbing-stairs/70-climbing-stairs.java
@@ -1,58 +1,28 @@
 class Solution {
-    
     public int climbStairs(int n) {
         
-//      Approch : 1
-        
         int[] dp = new int[n+1];
-        dp[0] = 1;
+        Arrays.fill( dp , -1 );
         
-        for( int i = 1; i <= n ; i++ ){
-            
-            int curr = 0;
-            for( int j = 1; j<= 2 ; j++ ){
-                
-                if( i - j >= 0 ){
-                    curr += dp[i - j];
-                }
-               
-            }
-            
-            dp[i] = curr;
-        }
-        
-        return dp[n];
-        
-        
-        
-        
-        
-        
-        
-        
-        
-//         int[] dp = new int[n+1];
-        
-//         Arrays.fill( dp , -1 );
-
-//         Approch : 2 
-//         ans( n , dp );
-        
-//         return dp[n];
+        return ans( n , dp );
         
     }
     
-//      public int ans( int n , int[] dp ) {
-         
-//         if( n == 0 ){
-//             return 1;
-//         }
-//         if( dp[n] != -1 ) return dp[n];
-         
-//         int a1 = ( n-1 >= 0 ) ? ans( n-1 , dp ) : 0;
-//         int a2 = ( n-2 >= 0 ) ? ans( n-2 , dp ) : 0;
+    int ans( int n , int[] dp ){
         
-//         return dp[n] = a1+a2;
+        if( n <= 1){
+            return 1;
+        }
+        
+        if( dp[n] != -1 ) return dp[n];
+        
+        int a1 = ans( n-1 , dp );
+        int a2 = ans( n-2 , dp );
+        
+        dp[n] = a1+a2;
+        
+        return dp[n];
+        
+    }
     
-//      }
 }
