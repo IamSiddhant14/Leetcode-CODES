b21347d7f29027772bdf50c18327411d659fa0bc
Siddhant Sharma <53156374+IamSiddhant14@users.noreply.github.com>
2022-06-23 02:06:22 +0530

    Time: 1 ms (88.42%), Space: 42 MB (87.25%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.java
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.java
@@ -1,25 +1,36 @@
 class Solution {
     public int minCostClimbingStairs(int[] cost) {
         
-        int[] dp = new int[cost.length+1];
+        if( cost.length == 0 ){
+            return 0;
+        }
         
+        if( cost.length == 1 ){
+            return cost[0];
+        }
+        
+        int[] dp = new int[cost.length+1];
         Arrays.fill( dp , -1 );
         
-        ans( dp , cost , 0 );
-        return Math.min( dp[0] , dp[1]);
+        ans( cost , dp , 0);
+        return Math.min( dp[0] , dp[1] );
+        
     }
     
-    int ans( int[] dp , int[] cost , int idx ){
-        
-        if( idx >= cost.length  ){
-            return Integer.MAX_VALUE;
+    int ans( int[] cost , int[] dp , int idx ){
+       
+        if( idx >= cost.length ){
+            return 0;
         }
         
         if( dp[idx] != -1 ) return dp[idx];
         
-        int a1 = (idx+1 <= cost.length-1 ) ? ans( dp , cost , idx+1) : 0 ;
-        int a2 = (idx+2 <= cost.length-1 ) ? ans( dp , cost , idx+2) : 0 ;
+        int a1 = ans( cost , dp , idx+1 );
+        int a2 = ans( cost , dp , idx+2 );
         
-        return dp[idx] = Math.min( a1 , a2 )+ cost[idx];
+        return dp[idx] = Math.min(a1 , a2) + cost[idx] ;
+        
     }
+    
+    
 }
