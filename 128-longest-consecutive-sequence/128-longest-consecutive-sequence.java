18673c9acb2304cda015f01b47d17e1d439c757d
Siddhant Sharma <53156374+IamSiddhant14@users.noreply.github.com>
2022-06-26 22:34:55 +0530

    Time: 69 ms (51.02%), Space: 79.8 MB (38.43%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.java
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.java
@@ -1,78 +1,36 @@
 class Solution {
-    
-    int getans( int[] nums ){
+    public int longestConsecutive(int[] nums) {
         
-        HashMap<Integer, Boolean> hm = new HashMap<>();
+        HashMap < Integer , Boolean > hm = new HashMap <>();
         
         for( int i = 0; i<nums.length ; i++ ){
-            hm.put( nums[i] , true );
+            hm.put( nums[i] , true);
         }
         
-        for( int i = 0; i<nums.length ; i++ ){
+        for( Integer a : hm.keySet() ){
             
-            if( hm.containsKey(nums[i]-1) == true ){
-                hm.put( nums[i] , false );
+            if( hm.containsKey(a-1) ){
+                hm.put( a , false );
             }
         }
         
         int max = 0;
-        
-        for( int ch : hm.keySet()){
+        for( Integer a : hm.keySet() ){
             
-            if( hm.get(ch) == true ){
+            if( hm.get(a) == true ){
                 
                 int count = 1;
                 
-                while( hm.containsKey(ch+count) ){
+                while( hm.containsKey(++a) ){
                     count++;
                 }
                 
-                max = Math.max( max , count);
-                
-                
+                max = Math.max( max , count );
             }
-            
         }
         
         return max;
         
         
-        
     }
-    
-    public int longestConsecutive(int[] nums) {
-        
-       return  getans( nums );
-        
-       // Approch : 1
-//         if( nums.length == 0 ) return 0;
-//         Arrays.sort( nums );
-        
-//         int left = 0 , max = 1;
-        
-//         while( left < nums.length ){
-            
-//             int right = left+1 , count = 1;
-            
-//             while( right < nums.length && (nums[right-1] == nums[right] || nums[right-1]+1 == nums[right])){
-                
-//                 if( nums[right-1] == nums[right]-1 ){
-//                     count++;
-//                 }
-                
-//                 right++;
-                
-//             }
-            
-//             max = Math.max( max , count);
-//             left = right;
-            
-            
-//         }
-        
-//         return max;
-        
-    }
-    
-    
 }
