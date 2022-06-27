9c93ebdb5e3532500b6f92f018a29f8e986c3960
Siddhant Sharma <53156374+IamSiddhant14@users.noreply.github.com>
2022-06-28 00:44:02 +0530

    Added solution - LeetHub

--- Marks of PCM - GFG/marks-of-pcm.java
+++ Marks of PCM - GFG/marks-of-pcm.java
@@ -41,46 +41,67 @@ class GfG
 
 class Solution
 {
-    class Marks{
-        int phy, chem, maths;
-        Marks(int phy, int chem, int maths){
-            this.phy = phy;
-            this.chem = chem;
-            this.maths = maths;
-        }
-    }
-    
-    class MarksCmp implements Comparator<Marks>{
-        public int compare(Marks s1, Marks s2){
-            if(s1.phy < s2.phy) return -1;
-            else if(s1.phy > s2.phy) return +1;
-            else {
-                if(s1.chem < s2.chem) return +1;
-                else if(s1.chem > s2.chem) return -1;
-                else {
-                    if(s1.maths < s2.maths) return -1;
-                    return +1;
-                }
-            }
-        }
-    }
-    
     public void customSort (int phy[], int chem[], int math[], int N)
     {
-        PriorityQueue<Marks> q = new PriorityQueue<>(new MarksCmp());
         
-        for(int i=0; i<N; i++){
-            Marks stud = new Marks(phy[i], chem[i], math[i]);
-            q.add(stud);
+        PriorityQueue <marks> pq = new PriorityQueue <> ( new Ma() );
+        
+        for( int i = 0; i<N ; i++ ){
+            marks m = new marks( phy[i] , chem[i] , math[i] );
+            pq.add(m);
         }
         
-        int idx = 0;
-        while(q.size() > 0){
-            Marks stud = q.remove();
-            phy[idx] = stud.phy;
-            chem[idx] = stud.chem;
-            math[idx] = stud.maths;
-            idx++;
+        for( int i = 0; i<N; i++ ){
+            
+            marks b = pq.remove();
+            
+            phy[i] = b.phy;
+            chem[i] = b.chem;
+            math[i] = b.math;
+            
         }
+        
     }
+    
+    static class Ma implements Comparator <marks> {
+        
+        public int compare ( marks a1 , marks a2 ){
+            
+            if( a1.phy < a2.phy ){
+                return -1;
+                
+            }else if( a2.phy < a1.phy ){
+                return 1;
+                
+            }else{
+                
+                if( a1.chem < a2.chem ){
+                    return 1;
+                    
+                }else if( a2.chem < a1.chem ){
+                    return -1;
+                    
+                }else{
+                    
+                    return a1.math- a2.math;
+                    
+                }
+                
+            }
+        }
+
+    }
+    
+    static class marks{
+            
+            int phy , chem , math = 0;
+            
+            marks(int p , int c , int m ){
+                this.phy = p;
+                this.chem = c;
+                this.math = m;
+            }
+    }
+    
+    
 }
