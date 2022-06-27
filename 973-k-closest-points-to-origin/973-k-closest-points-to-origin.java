642e9aa519f50136e450d3e4434651d046cbd1c3
Siddhant Sharma <53156374+IamSiddhant14@users.noreply.github.com>
2022-06-27 22:52:29 +0530

    Time: 51 ms (45.97%), Space: 120.2 MB (5.07%) - LeetHub

--- 973-k-closest-points-to-origin/973-k-closest-points-to-origin.java
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.java
@@ -1,42 +1,54 @@
 class Solution {
-    static class Pair{
-        public int x, y;
+    
+    public int[][] kClosest(int[][] points, int k) {
+        
+        PriorityQueue<pair> p = new PriorityQueue<> ( new Ecu() );
+        
+        for( int i = 0; i<points.length ; i++ ){
+            
+            pair a = new pair();
+            a.x = points[i][0];
+            a.y = points[i][1];
+            
+            p.add(a);
+    
+        }
+        
+        int[][] ans = new int[k][2];
+        
+        int j = 0;
+        while( j <k ){
+            pair c = p.remove();
+            ans[j][0] = c.x ;
+            ans[j][1] = c.y ;
+            
+            j++;
+        }
+        
+        return ans;
+        
     }
     
-    static class Euclidean implements Comparator<Pair> {
-        public int compare(Pair c1, Pair c2){
-            double d1 = Math.sqrt(c1.x * c1.x + c1.y * c1.y);
-            double d2 = Math.sqrt(c2.x * c2.x + c2.y * c2.y);
+    static class Ecu implements Comparator<pair> {
+        
+        public int compare ( pair a1 , pair a2 ){
             
-            if(d1 < d2){
+            double d1 = Math.sqrt( a1.x * a1.x + a1.y * a1.y );
+            double d2 = Math.sqrt( a2.x * a2.x + a2.y * a2.y );
+            
+           if(d1 < d2){
                 // Smaller Value -> Higher Priority
                 return -1;
             } else if(d1 > d2){
                 // Higher Value -> Lower Priority
                 return 1;
             } else return 0;
-        }    
+        }
     }
     
-    public int[][] kClosest(int[][] points, int k) {
-        PriorityQueue<Pair> q = new PriorityQueue<>(new Euclidean());
-        
-        for(int i=0; i<points.length; i++){
-            Pair p = new Pair();
-            p.x = points[i][0];
-            p.y = points[i][1];
-            q.add(p);
-        }
-        
-        int[][] res = new int[k][2];
-        int count = 0;
-        while(count < k){
-            Pair closest = q.remove();
-            res[count][0] = closest.x;
-            res[count][1] = closest.y;
-            count++;
-        }
-        
-        return res;
+    static class pair {
+        int x = 0 , y = 0;
     }
+    
+    
 }
