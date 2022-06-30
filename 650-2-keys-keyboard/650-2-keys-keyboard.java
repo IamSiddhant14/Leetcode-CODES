d72994181501437b2f5359bcee3e7ea4256ff668
Siddhant Sharma <53156374+IamSiddhant14@users.noreply.github.com>
2022-06-30 23:14:56 +0530

    Time: 448 ms (5.10%), Space: 348.1 MB (5.10%) - LeetHub

--- 650-2-keys-keyboard/650-2-keys-keyboard.java
+++ 650-2-keys-keyboard/650-2-keys-keyboard.java
@@ -1,83 +1,49 @@
 class Solution {
+    
     public int minSteps(int n) {
         
-//         if( n == 1 ) return 0;
-        
-//         long[][] dp = new long[2*n + 1 ][2*n + 1];
-        
-//         for( int i = 0; i<dp.length ; i++){
-//             for( int j = 0; j<dp.length ; j++ ){
-//                 dp[i][j] = -1;
-//             }
-//         }
-        
-//         return (int)( 1l + ans( 1 , 1 , n , dp ));
-        
-        if ( n == 1 ) return 0;
+        if( n == 1 ){
+            return 0;
+        }
         
         long[][] dp = new long[2*n+1][2*n+1];
         
-        for( int i = 0; i<= 2*n ; i++){
-            for( int j = 0; j<dp.length ; j++ ){
+        for( int i = 0; i<dp.length ; i++ ){
+            
+            for( int j = 0; j< dp.length ; j++ ){
                 dp[i][j] = -1;
             }
+            
         }
         
-        return (int)(ans(1, 1, n , dp )+1l);
+        return (int)(1l + findAns( 1 , 1, n , dp ));
         
+    }
+    
+    long findAns( int screen , int buffer , int dest , long[][] dp ){
+        
+        if( screen == dest ){
+            return 0;
+        }
+        
+        if( screen > dest ){
+            return Integer.MAX_VALUE;
+        }
+        
+        if( dp[screen][buffer] != -1 ) return  dp[screen][buffer];
+        
+        long copyPaste = 2l + findAns( screen * 2 , screen , dest , dp );
+        long paste = 1l + findAns( screen+buffer , buffer , dest , dp );
+        
+        dp[screen][buffer] = Math.min( copyPaste , paste );
+        
+        return dp[screen][buffer];
         
         
         
         
     }
-                     
-//     public long ans( int screen , int buffer , int dest , long[][] dp ){
-       
-//         if( screen == dest ){
-//            return 0;
-//         }
-
-//         if( screen > dest ){
-//            return Integer.MAX_VALUE;
-//         }   
-        
-//         if( dp[screen][buffer] != -1 ){
-//             return dp[screen][buffer];
-//         }
-        
-//         long copyPaste = 2l + ans( 2 * screen , screen , dest , dp );
-//         long Paste = 1l + ans( screen + buffer , buffer , dest , dp );
-        
-//         dp[screen][buffer] = Math.min( copyPaste , Paste );
-        
-//         return  dp[screen][buffer];
-        
-//     }
     
-       public long ans( int screen , int buffer , int dest , long[][] dp ){
-           
-           if( screen == dest ){
-               return 0;
-           }
-           
-           if( screen > dest ){
-               return Integer.MAX_VALUE;
-           }
-           
-        if( dp[screen][buffer] != -1 ){
-            return dp[screen][buffer];
-        }
-        
-           
-           long copyPaste = 2l + ans( 2*screen , screen , dest , dp );
-           long paste = 1l + ans( buffer+screen , buffer , dest , dp);
-           
-          dp[screen][buffer] = Math.min( copyPaste , paste );
-          return  dp[screen][buffer];
-           
-           
-       }
-                     
-                     
-                     
+    
+
 }
