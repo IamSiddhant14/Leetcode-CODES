eb3fbb7b1a8c2a385b5308db84c50c3356c237a8
Siddhant Sharma <53156374+IamSiddhant14@users.noreply.github.com>
2022-06-30 13:26:11 +0530

    Time: 0 ms (100.00%), Space: 40.9 MB (41.78%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.java
+++ 70-climbing-stairs/70-climbing-stairs.java
@@ -10,8 +10,12 @@ class Solution {
     
     int ans( int n , int[] dp ){
         
-        if( n <= 1){
+        if( n == 0){
             return 1;
+        }
+        
+        if( n < 0 ){
+            return 0;
         }
         
         if( dp[n] != -1 ) return dp[n];
