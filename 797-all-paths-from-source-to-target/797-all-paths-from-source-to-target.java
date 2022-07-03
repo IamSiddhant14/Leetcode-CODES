cd75923066d9808a16f6a9897c04a04078c28a10
Siddhant Sharma <53156374+IamSiddhant14@users.noreply.github.com>
2022-07-03 19:07:52 +0530

    Time: 5 ms (52.68%), Space: 54.9 MB (63.25%) - LeetHub

--- 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.java
+++ 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.java
@@ -2,13 +2,17 @@ class Solution {
     
     public void findAns( int src , int dest , int[][] graph , boolean[] vis , List<Integer> pair , List<List<Integer>> pairs){
         
-        if( vis[src] == true ) return;
+        if( vis[src] == true ) return;//loop detection
         
         vis[src] = true;
         pair.add(src);
         
         if( src == dest ){
-            pairs.add(new ArrayList(pair));
+            pairs.add(new ArrayList(pair));// Deep copy is essential or else it will be ointing to the same node in which we are removing element
+            vis[src] = false;
+            pair.remove(pair.size() -1);
+            return;
+            
         };
         
         for( int v : graph[src]){
