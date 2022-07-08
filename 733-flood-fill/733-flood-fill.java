eafe72d0a066f586f4c638b709664df955d63832
Siddhant Sharma <53156374+IamSiddhant14@users.noreply.github.com>
2022-07-08 20:12:24 +0530

    Time: 1 ms (90.26%), Space: 48 MB (57.54%) - LeetHub

--- 733-flood-fill/733-flood-fill.java
+++ 733-flood-fill/733-flood-fill.java
@@ -1,31 +1,29 @@
 class Solution {
+    public int[][] floodFill(int[][] image, int sr, int sc, int color ) {
+        
+         findAns( image , sr , sc , color , image[sr][sc]);
+        return image;
+    }
     
-    public void DFS( int[][] image, int sr, int sc, int color , int ogColor ){
+    public void findAns(int[][] image, int sr, int sc, int color , int prev){
         
         if( sr< 0 || sc < 0 || sr >= image.length || sc >= image[0].length ){
             return;
         }
         
-        if( image[sr][sc] != ogColor ){
-            return;
-        }
-        
+        if( image[sr][sc] == color ) return;
+            
+        if( image[sr][sc] == prev ){
+            
         image[sr][sc] = color;
+            
+        findAns( image , sr , sc-1 , color , prev);
+        findAns( image , sr-1 , sc , color , prev);
+        findAns( image , sr , sc+1 , color , prev);
+        findAns( image , sr+1 , sc , color , prev);
         
-        DFS(image , sr , sc-1, color , ogColor );
-        DFS(image , sr-1 , sc, color , ogColor );
-        DFS(image , sr , sc+1, color , ogColor );
-        DFS(image , sr+1 , sc, color , ogColor );
-      
-    }
-    
-    
-    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        
-        if( image[sr][sc] == color ) return image ;
-        
-        DFS( image , sr , sc , color , image[sr][sc]);
-        return image;
-        
+        }
+
+
     }
 }
