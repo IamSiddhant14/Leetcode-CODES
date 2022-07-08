6ab6590582146f49eca168f77eab6c1030940dc5
Siddhant Sharma <53156374+IamSiddhant14@users.noreply.github.com>
2022-07-09 03:22:34 +0530

    Time: 183 ms (53.77%), Space: 224.2 MB (31.62%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
@@ -1,64 +1,54 @@
 class Graph{
     
-    ArrayList<Integer>[] adj ;
+    ArrayList<Integer> [] adj ;
     
     Graph(int n){
         
         adj = new ArrayList[n];
         
-        for( int i = 0; i<n ; i++ ){
+        for( int i = 0; i<n ; i++){
             adj[i] = new ArrayList<>();
         }
         
     }
     
-    public void addEdge(int src , int dest ){
-        
-        adj[src].add(dest);
-        adj[dest].add(src);
-        
+    public void addEdge( int src , int dest ){
+       adj[src].add(dest);
+       adj[dest].add(src);
     }
     
 }
 
 
-
-
 class Solution {
-    
     public boolean validPath(int n, int[][] edges, int src, int dest) {
-        
+        //We are been given an edge list we need to convert it to adjacncy list so to do so
         Graph g = new Graph(n);
         
         for( int i = 0; i<edges.length ; i++ ){
-            g.addEdge( edges[i][0] , edges[i][1]);
+            g.addEdge(edges[i][0] , edges[i][1] );
         }
         
         boolean [] vis = new boolean[n];
-        
-        return DFS( g , src , dest , vis );
-        
+        return DFS(src, dest, g , vis );
         
     }
     
-    public boolean DFS( Graph g , int src ,int dest , boolean[] vis ){
-        
+    
+    public boolean DFS( int src , int dest , Graph g , boolean[] vis ){
+
         if( src == dest ) return true;
-        
-        if( vis[src] == true) return false;
+        if( vis[src] == true ) return false;
         
         vis[src] = true;
-        
-        for( int v : g.adj[src]){
+        for( int v : g.adj[src] ){
             
-            if( DFS( g , v , dest , vis) == true ) return true;
-            
+            if( DFS(v , dest , g , vis) == true ) return true;
         }
-        
+    
         return false;
-        
+    
+    
+    
     }
-    
-    
-    
 }
