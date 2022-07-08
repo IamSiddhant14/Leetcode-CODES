2c710bff8737e9aedb44fe87fa8f1552cd30df62
Siddhant Sharma <53156374+IamSiddhant14@users.noreply.github.com>
2022-07-09 05:22:19 +0530

    Time: 11 ms (17.76%), Space: 55.4 MB (24.98%) - LeetHub

--- 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.java
+++ 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.java
@@ -1,45 +1,32 @@
 class Solution {
+    public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
+       
+        List<List<Integer>> pairs = new ArrayList<>();
+        List<Integer> pair = new ArrayList<>();
+        
+        boolean[] vis = new boolean[graph.length];
+        DFS( 0 , graph.length-1 , graph , vis , pair , pairs );
+        
+        return pairs;
+    }
     
-    public void findAns( int src , int dest , int[][] graph , boolean[] vis , List<Integer> pair , List<List<Integer>> pairs){
+    public void DFS( int src , int dest , int[][] graph , boolean[] vis , List<Integer> pair , List<List<Integer>> pairs  ){
         
-        if( vis[src] == true ) return;//loop detection
-        
-        vis[src] = true;
+        if( vis[src] == true ) return ;
+                
         pair.add(src);
+        vis[src] = true;
         
         if( src == dest ){
-            pairs.add(new ArrayList(pair));// Deep copy is essential or else it will be ointing to the same node in which we are removing element
-            vis[src] = false;
-            pair.remove(pair.size() -1);
-            return;
-            
+            pairs.add(new ArrayList(pair));// Deep copy is essential or else it will be ointing to the same node in which we are removing element            
         };
         
         for( int v : graph[src]){
-            findAns( v , dest , graph , vis , pair , pairs );
+            DFS( v , dest , graph , vis , pair , pairs );
         }
         
         vis[src] = false;
-        pair.remove(pair.size() -1);
-        return;
-
-    }
-    
-    
-    public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
-        
-        List<Integer> Pair = new ArrayList<>();
-        List<List<Integer>> Pairs = new ArrayList<>();
-        
-        boolean[] vis = new boolean[graph.length];
-        
-        findAns( 0 , graph.length -1 , graph , vis , Pair , Pairs );
-        return Pairs;
+        pair.remove( pair.size() -1 );
         
     }
-    
-    
-    
-    
 }
-
