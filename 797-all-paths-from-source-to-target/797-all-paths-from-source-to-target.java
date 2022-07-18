0d68c00d4b01c543e48df21844a852159ca06437
Siddhant Sharma <53156374+IamSiddhant14@users.noreply.github.com>
2022-07-19 03:38:20 +0530

    Time: 3 ms (87.87%), Space: 54.8 MB (68.69%) - LeetHub

--- 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.java
+++ 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.java
@@ -1,36 +1,32 @@
-
 class Solution {
     public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
         
-        boolean[] vis = new boolean[graph.length];
-        List<List<Integer>> ans = new ArrayList<>();
+        boolean [] vis = new boolean[graph.length];
         
         List<Integer> temp = new ArrayList<>();
-        DFS( vis , graph , 0 , graph.length-1 , temp , ans );
+        List<List<Integer>> ans = new ArrayList<>();
         
+        DFS( vis , graph , temp , ans , 0 , graph.length-1);
         return ans;
-        
     }
     
-    public void DFS( boolean[] vis , int[][] graph , int src , int dest , List<Integer> temp , List<List<Integer>> ans ){
+    public void DFS( boolean[] vis , int[][] graph , List<Integer> temp , List<List<Integer>> ans , int src , int dest ){
         
         if( vis[src] == true ) return ;
         
         temp.add(src);
+        vis[src] = true;
         
         if( src == dest ){
             ans.add(new ArrayList<>(temp));
-           
         }
-        vis[src] = true;
-
-        for( int ele : graph[src]){
-            DFS( vis , graph , ele , dest , temp , ans);
+        
+        for( int ele : graph[src] ){
+            DFS( vis , graph , temp , ans , ele , dest );
         }
         
         vis[src] = false;
         temp.remove(temp.size()-1);
-         return;
-
-    }  
+        
+    }
 }
