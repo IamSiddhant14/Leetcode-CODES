047095f6314a069d92ea74f4593005bae31020d4
Siddhant Sharma <53156374+IamSiddhant14@users.noreply.github.com>
2022-07-21 18:17:17 +0530

    Time: 256 ms (33.96%), Space: 216 MB (39.11%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
@@ -1,53 +1,49 @@
 class Graph{
     
-    ArrayList<Integer> [] adj ;
+    ArrayList<Integer> [] adj;
     
     Graph(int n){
         
         adj = new ArrayList[n];
         
-        for( int i = 0; i<n ; i++){
-            adj[i] = new ArrayList<>();
+        for( int i = 0; i<n ; i++ ){
+            adj[i] = new ArrayList<>();  
         }
-        
     }
     
     public void addEdge( int src , int dest ){
-       adj[src].add(dest);
-       adj[dest].add(src);
+        adj[src].add(dest);
+        adj[dest].add(src);
     }
     
+    public boolean DFS(int src , boolean[] vis , int dest ){
+        
+        if( src == dest ) return true;
+        if( vis[src] == true ) return false;
+        
+        vis[src] = true;
+        
+        for( int ele : adj[src]){
+            if( DFS( ele , vis , dest ) == true ) return true;
+        }
+        
+        return false;
+    }
 }
-
 
 class Solution {
     
-    public boolean validPath(int n, int[][] edges, int src, int dest) {
-        //We are been given an edge list we need to convert it to adjacncy list so to do so
+    public boolean validPath(int n, int[][] edges, int source, int destination) {
+        
         Graph g = new Graph(n);
         
-        for( int i = 0; i<edges.length ; i++ ){
-            g.addEdge(edges[i][0] , edges[i][1] );
+        for( int i = 0; i<edges.length; i++ ){
+            g.addEdge(edges[i][0] , edges[i][1]);
         }
         
-        boolean [] vis = new boolean[n];// This is done to avoid loop
-        return DFS(src, dest, g , vis );
+        boolean[] vis = new boolean[n];
         
-    }
-    
-    
-    public boolean DFS( int src , int dest , Graph g , boolean[] vis ){
-
-        if( src == dest ) return true;
-        if( vis[src] == true ) return false;// This is done to avoid loop
+        return g.DFS( source , vis , destination );
         
-        vis[src] = true;
-        for( int v : g.adj[src] ){
-            
-            if( DFS(v , dest , g , vis) == true ) return true;
-        }
-    
-        return false;
-    
     }
 }
