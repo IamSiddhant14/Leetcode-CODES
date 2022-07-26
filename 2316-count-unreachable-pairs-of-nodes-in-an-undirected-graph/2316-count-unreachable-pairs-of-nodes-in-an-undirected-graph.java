1c436a581230790a58a1deb486962e4500e597f7
Siddhant Sharma <53156374+IamSiddhant14@users.noreply.github.com>
2022-07-26 18:15:55 +0530

    Time: 119 ms (40.43%), Space: 145.4 MB (82.93%) - LeetHub

--- 2316-count-unreachable-pairs-of-nodes-in-an-undirected-graph/2316-count-unreachable-pairs-of-nodes-in-an-undirected-graph.java
+++ 2316-count-unreachable-pairs-of-nodes-in-an-undirected-graph/2316-count-unreachable-pairs-of-nodes-in-an-undirected-graph.java
@@ -1,16 +1,18 @@
 class Graph{
     
-   List<Integer> [] adj;
+    ArrayList<Integer>[] adj;
     
-    Graph( int n ){
+    Graph(int n){
+        
         adj = new ArrayList[n];
         
-        for( int i = 0; i<n ; i++ ){
+        for( int i=0; i<n ; i++ ){
             adj[i] = new ArrayList<>();
         }
+        
     }
     
-    public void addEdge( int src , int dest ){
+    public void addEdge(int src , int dest ){
         adj[src].add(dest);
         adj[dest].add(src);
     }
@@ -19,45 +21,45 @@ class Graph{
 
 class Solution {
     
-    public void DFS( List<Integer> temp , Graph g , int src , boolean[] vis){
-        
-        if( vis[src] == true ) return;
-        
-        vis[src] = true;
-        temp.add(src);
-        
-        for( int ele : g.adj[src] ){
-            DFS( temp , g , ele , vis );
-        }
-        
-    }
     
+   public static void DFS( List<Integer> temp , boolean[] vis , Graph g , int src ){
+
+      if(vis[src] == true) return;
+      vis[src] = true;
+
+      temp.add(src);
+      
+      for( int ele : g.adj[src] ){
+         DFS( temp , vis, g , ele );
+      }
+
+   }
     
     
     public long countPairs(int n, int[][] edges) {
         
         Graph g = new Graph(n);
         
-        for(int i = 0; i<edges.length ; i++ ){
-            g.addEdge(edges[i][0],edges[i][1]);
+        for( int i =0; i<edges.length ;i++ ){
+            g.addEdge(edges[i][0] , edges[i][1]);
         }
-    
+        
         long count = 0;
-        boolean[] vis = new boolean[n];
         
-        for( int i=0 ; i<n ; i++ ){
-            
-            if( vis[i] == false ){
-                
-                List<Integer> temp = new ArrayList<>();
-                DFS( temp , g , i , vis);
-                
-                count +=  (((n*1l) - temp.size() ) * temp.size() ) ;
-            }
-            
-        }
+      boolean[] vis = new boolean[n];
+
+      for( int i=0 ; i<n ; i++ ){
+         
+         if( vis[i] == false ){
+
+            List<Integer> temp = new ArrayList<Integer>();
+            DFS(temp , vis , g , i );
+            count =   count +=  (((n*1l) - temp.size() ) * temp.size() ) ;
+
+         }
+      }
         
-        return count / 2l ;
-        
+      return count / 2l ;
+
     }
 }
