5e2cddb40258a530f0f85088f08341e12d175e52
Siddhant Sharma <53156374+IamSiddhant14@users.noreply.github.com>
2022-08-06 22:38:05 +0530

    Time: 5 ms (62.92%), Space: 42.5 MB (31.53%) - LeetHub

--- 767-reorganize-string/767-reorganize-string.java
+++ 767-reorganize-string/767-reorganize-string.java
@@ -1,85 +1,71 @@
 class Solution {
-    
-    static class Pair{
-        
-        char ch ;
-        int freq ;
-        
-        Pair( char a , int b ){
-            this.ch = a;
-            this.freq = b;
-        }
-    }
-    
-    static class FreqComp implements Comparator<Pair> {
-        
-        public int compare ( Pair a, Pair b ){
-            
-            if( a.freq == b.freq ){
-                return a.ch - b.ch ;
-            }
-            
-            return b.freq - a.freq ;
-        }
-    }
-    
-    
+
     public String reorganizeString(String s) {
         
-        HashMap <Character , Integer> hm = new HashMap<>();
-        
-        for( int i = 0; i<s.length() ; i++){
-    
-            char a = s.charAt(i);
-            hm.put( a , hm.getOrDefault( a , 0 ) + 1 );
-            
-        }
-        
-        PriorityQueue <Pair> pq = new PriorityQueue<>(new FreqComp());
-        
-        for( Character c : hm.keySet() ){
-            
-            int freq = hm.get(c) ;
-            Pair p = new Pair( c , freq );
-            
-            pq.add(p);
-            
-        }
-        
-        StringBuilder sb = new StringBuilder();
-        
-        while( pq.size() > 0 ){
-            
-            Pair a = pq.remove();
-            
-            sb.append(a.ch);
-            a.freq = a.freq-1;
-            
-            if( pq.size() == 0 && a.freq > 0 ){
-                return "";
-            }
-            
-            if( pq.size()> 0 ){
-                
-                Pair b = pq.remove();
-                sb.append(b.ch);
-                
-                if( b.freq > 1 ){                  
-                     b.freq = b.freq-1;
-                     pq.add(b);
-                }
-                
-                if( a.freq > 0 ){
-                    pq.add(a);
-                }
+        HashMap<Character, Integer> hm = new HashMap<>();
 
-            }
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            hm.put(ch, hm.getOrDefault(ch, 0) + 1);
+        }
+
+        PriorityQueue<Pair> pq = new PriorityQueue<>(new Ans());
+
+        for (Character ch : hm.keySet()) {
+            int v = hm.get(ch);
+            Pair p = new Pair(ch, v);
+
+            pq.add(p);
+        }
+
+        StringBuilder sb = new StringBuilder();
+        while (pq.size() > 0) {
             
+            Pair p1 = pq.remove();
+            sb.append(p1.ch);
+
+            if (pq.size() == 0) {
+                if( p1.freq > 1 ) return "";
+            }
+
+            if (pq.size() > 0) {
+                
+                Pair p2 = pq.remove();
+                sb.append(p2.ch);
+
+                if (p2.freq > 1) {
+                    pq.add(new Pair(p2.ch, p2.freq - 1));
+                }
+            }
+
+            if (p1.freq > 1) {
+                pq.add(new Pair(p1.ch, p1.freq - 1));
+            }
         }
         
         String ans = sb.toString();
         return ans;
         
-        
+    }
+
+    static class Pair {
+        char ch;
+        int freq;
+
+        Pair(char c, int a) {
+            this.ch = c;
+            this.freq = a;
+        }
+    }
+
+    static class Ans implements Comparator<Pair> {
+
+        public int compare(Pair a1, Pair a2) {
+            if (a1.freq == a2.freq) {
+                return a1.ch - a2.ch;
+            }
+
+            return a2.freq - a1.freq;
+        }
     }
 }
